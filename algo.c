dc19f8cc646bcbd1ba352146a79728822e724771
hhxxttxsggg <16502221+hhxxttxsggg@user.noreply.gitee.com>
2026-03-23 15:43:39 +0800

    对于一个递归函数 w(a,b,c)
    如果 a≤0 或 b≤0 或 c≤0 就返回值 1。
    如果 a>20 或 b>20 或 c>20 就返回 w(20,20,20)
    如果 a<b 并且 b<c 就返回 w(a,b,c−1)+w(a,b−1,c−1)−w(a,b−1,c)。
    其它的情况就返回 w(a−1,b,c)+w(a−1,b−1,c)+w(a−1,b,c−1)−w(a−1,b−1,c−1)
    这是个简单的递归函数，但实现起来可能会有些问题。当 a,b,c 均为 15 时，调用的次数将非常的多。你要想个办法才行。
    好吧，这题我想说的是格式，最初我以为l就够用了

--- algo.c
+++ algo.c
@@ -1,26 +1,35 @@
 #include <stdio.h>
 #include <stdlib.h>
-long  seqnum(int n,long b[]) {//防止越界，不用int
+long long w(long long a,long long b,long long c, long long arr[21][21][21]) {
 
-    if (n==1) return b[1]=1;
-    for (int i=1 ;i<=n/2;i++) {
-        if (b[i]==0) {
-            b[i]=seqnum(i,b);
-            b[n]+=b[i];
-        }
-        else {
-            b[n]+=b[i];
-        }
+    if (a<=0||b<=0||c<=0) {
+        return  arr[0][0][0];
     }
-     b[n]=b[n]+1;
-
-    return b[n];
+    else if (a>20||b>20||c>20) {
+        return w(20,20,20,arr);
+    }
+    else if (arr[a][b][c]) return arr[a][b][c];
+    else if (a<b&&b<c) {
+        //如果 a<b 并且 b<c 就返回 w(a,b,c−1)+w(a,b−1,c−1)−w(a,b−1,c)。
+        arr[a][b][c]= w(a,b,c-1,arr)+w(a,b-1,c-1,arr)-w(a,b-1,c,arr);
+        return arr[a][b][c];
+    }
+    else {
+        //w(a−1,b,c)+w(a−1,b−1,c)+w(a−1,b,c−1)−w(a−1,b−1,c−1)
+         arr[a][b][c]=w(a-1,b,c,arr)+w(a-1,b-1,c,arr)+w(a-1,b,c-1,arr)-w(a-1,b-1,c-1,arr);
+        return arr[a][b][c];
+    }
 }
 int main() {
-    int n=0;
-    scanf("%d",&n);
-    long*c=(long*)calloc(n+1,sizeof(long));
-printf("%ld",seqnum(n,c));
-    free(c);
+    long long arr[21][21][21]={0};
+    long long a=0,b=0,c=0;
+    arr[0][0][0]=1;
+    while (1) {
+        scanf("%lld%lld%lld",&a,&b,&c);
+        if (a==-1&&b==-1&&c==-1) break;
+        long long s=w(a,b,c,arr);
+        printf("w(%lld, %lld, %lld) = %lld\n",a,b,c,s);
+
+    }
     return 0;
 }
