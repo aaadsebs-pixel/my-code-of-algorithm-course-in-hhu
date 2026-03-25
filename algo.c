057fe5df100a7890bbad10ae7310e5d9465392e5
hhxxttxsggg <16502221+hhxxttxsggg@user.noreply.gitee.com>
2026-03-25 15:11:35 +0800

    这是一道当时转专业考试的第3题，一年了仍然记忆尤新，写的过程也出现了很多的语法错误，经过尝试，目前仍然没有解决浮点数的问题，但是开始能够debug，希望我能把算法题都掌握，加油
    将 1,2,…,9 共 9 个数分成三组，分别组成三个三位数，且使这三个三位数的比例是 A:B:C，试求出所有满足条件的三个三位数，若无解，输出 No!!!。
    
    输入格式
    三个数，A,B,C。
    
    输出格式
    若干行，每行 3 个数字。按照每行第一个数字升序排列。

--- algo.c
+++ algo.c
@@ -1,77 +1,33 @@
 #include <stdio.h>
-#define MAX_LEN 200
-
-int seq[MAX_LEN];
-static void dfs123(int c1, int c2, int c3, int depth, int total_len) {
-    if (depth == total_len) {
-        for (int i = 0; i < total_len; ++i) {
-            if (i) printf(" ");
-            printf("%d", seq[i]);
-        }
-        printf("\n");
-        return;
-    }
-
-    // 按 1,2,3 的顺序尝试，输出天然为字典序
-    if (c1 > 0) {
-        seq[depth] = 1;
-        dfs123(c1 - 1, c2, c3, depth + 1, total_len);
-    }
-    if (c2 > 0) {
-        seq[depth] = 2;
-        dfs123(c1, c2 - 1, c3, depth + 1, total_len);
-    }
-    if (c3 > 0) {
-        seq[depth] = 3;
-        dfs123(c1, c2, c3 - 1, depth + 1, total_len);
-    }
-}
-void print_permutations_123(int c1, int c2, int c3) {
-    if (c1 < 0 || c2 < 0 || c3 < 0) return;
-
-    int total_len = c1 + c2 + c3;
-    if (total_len > MAX_LEN) return;
-
-    dfs123(c1, c2, c3, 0, total_len);
-}
-
-int combination(int n, int k,int arr[11][11]) {
-    if (k == 0 || n == k) {
-        return 1;
-    }
-    if (arr[n][k] !=0) {
-        return arr[n][k];
-    }
-    else if (n<k) return 0;
-    arr[n][k] = combination(n - 1, k - 1,arr) + combination(n - 1, k,arr);
-    return arr[n][k];
+#include <string.h>
+int arr[10]={0};
+void get(int a) {
+    arr[a%10]=1;
+    arr[a/10%10]=1;
+    arr[a/100]=1;
 }
 int main() {
-    int n;
-    int m=0;
-    int arr[11][11]={0};
-    scanf("%d",&n);
-    if (n<10||n>30) {
-        printf("%d",m);
-        return 0;
+    int a,b,c;
+    int d=0,e=0,count=0;
+    scanf("%d%d%d",&a,&b,&c);
+    for (int i=123;i<988;i++) {
+        if(i*b%a||i*c%a) continue;
+        d=i*b/a;
+        e=i*c/a;
+        if (d>987||e>987) break;
+        get(i);
+        get(d);
+        get(e);
+        int j=1;
+        for (;j<10;j++) {
+            if (arr[j]==0) break;
+        }
+if (j==10) {
+    printf("%d %d %d\n",i,d,e);
+    count++;
+}
+memset(arr,0,sizeof(arr));
     }
-
-    int l=n-10;
-    int ans[21]={0};
-    for (int i=l/2;i>=0;i--) {
-        int x=l-2*i;
-        if (x>10) break;
-        ans[n-10]+=combination(10,x,arr)*combination(10-x,i,arr);
-    }
-    int a=ans[n-10];
-    printf("%d\n",a);
-    for (int i=l/2;i>=0;i--) {
-        int x=l-2*i;
-        if (x>10) break;
-       // ans[n-10]+=combination(10,x,arr)*combination(10-x,i,arr);
-        print_permutations_123(10-x-i,x,i);
-    }
-
-
+    if (count==0) printf("No!!!");
     return 0;
 }
