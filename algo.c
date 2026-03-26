6fb5cdb24a497f7950ba302e27832e4e0e59fa85
hhxxttxsggg <16502221+hhxxttxsggg@user.noreply.gitee.com>
2026-03-26 17:38:38 +0800

    这题算是一个递归？选还是不是体现回溯可能，但是我没有使用复原现场的方法，好的不说了，去学操作系统了

--- algo.c
+++ algo.c
@@ -1,58 +1,73 @@
-#include <stdio.h>
-#include <math.h>
-#include <stdlib.h>
-int get(int arr[],int n) {
-    for (int i=0;i<n;i++) {
-        if (arr[i]==arr[n]||abs(arr[i]-arr[n])==abs(i-n)) return 0;
+#define max(a,b) ((a)>(b)?(a):(b))
+#define min(a,b) ((a)<(b)?(a):(b))
+#include<stdio.h>
+#include<stdlib.h>
+
+
+int  fun(int x,int arr[],int mdp,int mt,int nm,int sum) {
+    if (x>mdp-1) {
+        mt=max(mt,sum-nm);
+        return mt;
     }
-    return 1;
+    int result=fun(x+1,arr, mdp, mt, nm, sum);
+    if (nm+arr[x]<=sum/2) {
+        nm+=arr[x];
+        result=min(fun(x+1,arr, mdp, mt, nm, sum),result);
+    }
+    return result;
+
 }
 int main() {
-    int n;
-    scanf("%d", &n);
-int*jp=(int*)calloc(n,sizeof(int));
-    if (jp==NULL) {
-        free(jp);
-        return 0;
+    int a,b,c,d;
+    scanf("%d %d %d %d",&a,&b,&c,&d);
+    int*a1=(int*)calloc(a,sizeof(int));
+    int*b1=(int*)calloc(b,sizeof(int));
+    int*c1=(int*)calloc(c,sizeof(int));
+    int*d1=(int*)calloc(d,sizeof(int));
+    if(a1==NULL || b1==NULL || c1==NULL || d1==NULL) {
+        free(a1);
+        free(b1);
+        free(c1);
+        free(d1);
+        return -1;
     }
-    int count=0;
-    int i=0;
-    int j=0;
-    while (i>=0) {
-
-        for (;j<n;j++) {
-            jp[i]=j;
-            if (get(jp,i))break;
-        }
-        if (j<n) {
-            if (i==n-1) {
-                count++;
-                if (count<4) {
-                    for (int m=0;m<n;m++) {
-                        printf("%d ",jp[m]+1);
-                    }
-                    printf("\n");
-                }
-                    jp[i]=0;
-                i--;
-                j=jp[i]+1;
-
-            }
-            else  {
-                jp[i]=j;
-                i++;
-                j=0;
-            }
-        }
-        else {
-            i--;
-            j=jp[i]+1;
-        }
-
-
-
+    for(int i=0;i<a;i++) {
+        scanf("%d",&a1[i]);
     }
-    printf("%d",count);
-    free(jp);
+    for(int j=0;j<b;j++) {
+        scanf("%d",&b1[j]);
+    }
+    for(int k=0;k<c;k++) {
+        scanf("%d",&c1[k]);
+    }
+    for(int l=0;l<d;l++) {
+        scanf("%d",&d1[l]);
+    }
+    int ma=0,mb=0,mc=0,md=0;
+    int mdp=0,mt=0,nm=0,sum=0;
+    for(int i=0;i<a;i++) {
+        sum+=a1[i];
+    }
+    ma=fun(0,a1,a,ma,nm,sum);
+     mdp=0;mt=0;nm=0;sum=0;
+    for(int i=0;i<b;i++) {
+        sum+=b1[i];
+    }
+    mb=fun(0,b1,b,mb,nm,sum);
+    mdp=0;mt=0;nm=0;sum=0;
+    for(int i=0;i<c;i++) {
+        sum+=c1[i];
+    }
+    mc=fun(0,c1,c,mc,nm,sum);
+    mdp=0;mt=0;nm=0;sum=0;
+    for(int i=0;i<d;i++) {
+        sum+=d1[i];
+    }
+    md=fun(0,d1,d,md,nm,sum);
+    printf("%d\n",ma+mb+mc+md);
+    free(a1);
+    free(b1);
+    free(c1);
+    free(d1);
     return 0;
 }
