1b864b117b96a0c0f51b89ae3f1c82dfbb37fa8f
hhxxttxsggg <16502221+hhxxttxsggg@user.noreply.gitee.com>
2026-03-29 21:59:22 +0800

    好的，这是在ai的帮助下第一次写那么快的，这样的速度，我的那个8作业应该能交上， 然后我发现了一个很严重的问题我没有写过注释，这可能是因为现在的这个阅读对象的人还是我吧，然后就是学会了申请二维动态数组的方法 int(*arr)[m]=(int(*)[m])calloc(n*m,sizeof(int)) 对数据的处理确实是一件困难的事情，今天一共完成了3个题

--- algo.c
+++ algo.c
@@ -1,43 +1,74 @@
 #include <stdio.h>
-#include <string.h>
-int arr[100][100]={0};
-int brr[100][100]={0};
-int n=0,m=0;
-void change(char str[], int k,int m) {
-    for (int i = 0; i < m; i++) {
-        arr[k][i] = str[m - 1 - i] - '0';
+#include <stdlib.h>
+int n,m;
+int l[8]={0,1,1,1,0,-1,-1,-1};
+int w[8]={1,1,0,-1,-1,-1,0,1};
+void change(int arr[n][m],int brr[n][m],int x,int y) {
+    if (arr[x][y]) {
+        brr[x][y] =66;
     }
-}
-void flood(int x, int y,int arr[100][100],int brr[100][100]) {
-    int dx[] = {-1, 1, 0, 0};
-    int dy[] = {0, 0, -1, 1};
-    if(x<0 || x>n || y<0 || y>m) return;
-    if(brr[x][y]||arr[x][y]==0) return;
     else {
-        brr[x][y] =1;
-            for(int i=0;i<4;i++) {
-                int nx=x+dx[i];
-                int ny=y+dy[i];
-                flood(nx,ny,arr,brr);
+        for (int i=0;i<8;i++) {
+            int dx = x+l[i];
+            int dy = y+w[i];
+            if (dx>=0 && dx<n && dy>=0 && dy<m) {
+                brr[x][y]+=arr[dx][dy];
             }
         }
     }
-
-int main() {
-    scanf("%d%d",&n,&m);
-
-    int count=0;
-    for(int i=0;i<n;i++) {
-        char str[100];
-        scanf("%s",str);
-        change(str,i,m);
-    }
-    for(int i=0;i<n;i++) {
-        for(int j=0;j<m;j++) {
-            if(arr[i][j]&&brr[i][j]==0) count++;
-            flood(i,j,arr,brr);
+}
+void flood(int x, int y,int brr[n][m],int crr[n][m]) {
+    if(x<0 || x>=n || y<0 || y>=m) return;
+    if(brr[x][y]==66||crr[x][y]) return;
+    else {
+        crr[x][y] =1;
+        if (brr[x][y]==0) {
+            for(int i=0;i<8;i++) {
+                int nx=x+l[i];
+                int ny=y+w[i];
+                flood(nx,ny,brr,crr);
+            }
         }
     }
-    printf("%d",count);
+}
+
+
+
+int main() {
+    scanf("%d %d",&n,&m);
+    int(*arr)[m] = (int(*)[m])calloc(n*m,sizeof(int));
+    int(*brr)[m] = (int(*)[m])calloc(n*m,sizeof(int));
+    int(*crr)[m] = (int(*)[m])calloc(n*m,sizeof(int));
+    if(arr==NULL||brr==NULL||crr==NULL) return -1;
+    for(int i=0;i<n;i++) {
+        for(int j=0;j<m;j++) {
+            scanf("%d",&arr[i][j]);
+        }
+    }
+    for(int i=0;i<n;i++) {
+        for(int j=0;j<m;j++) change(arr,brr,i,j);
+    }
+    int count=0;
+    for(int k=0;k<n;k++){
+        for(int j=0;j<m;j++) {
+            if (crr[k][j]==0&&brr[k][j]==0) {
+                count++;
+                flood(k,j,brr,crr);
+            }
+        }
+    }
+    for(int k=0;k<n;k++){
+        for(int j=0;j<m;j++) {
+            if (crr[k][j]==0&&brr[k][j]!=66) {
+                count++;
+                flood(k,j,brr,crr);
+            }
+        }
+    }
+    printf("%d\n",count);
+
+    free(arr);
+    free(brr);
+    free(crr);
     return 0;
 }
