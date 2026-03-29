e3c3d6a8f1bbf337c64342bfe6eb87141f1068d9
hhxxttxsggg <16502221+hhxxttxsggg@user.noreply.gitee.com>
2026-03-29 14:17:30 +0800

    24点的问题，最后这点是ai生成的，因为我脑子里只有一个想法保存，但是具体怎么做不知道，这也纠正我什么都想要写入一个函数里面，然后main中一调就好了，然后看到科软今年的情况，快吓哭了，快学习，快学习

--- algo.c
+++ algo.c
@@ -1,73 +1,70 @@
-#define max(a,b) ((a)>(b)?(a):(b))
-#define min(a,b) ((a)<(b)?(a):(b))
-#include<stdio.h>
-#include<stdlib.h>
+#include <stdio.h>
 
+int stepA[4], stepB[4], stepR[4];
+char stepOp[4];
+int stepCount = 0;
 
-int  fun(int x,int arr[],int mdp,int mt,int nm,int sum) {
-    if (x>mdp-1) {
-        mt=max(mt,sum-nm);
-        return mt;
-    }
-    int result=fun(x+1,arr, mdp, mt, nm, sum);
-    if (nm+arr[x]<=sum/2) {
-        nm+=arr[x];
-        result=min(fun(x+1,arr, mdp, mt, nm, sum),result);
-    }
-    return result;
-
+int equal(int a,int b) {
+    if (a==b) return 1;
+    else return 0;
 }
-int main() {
-    int a,b,c,d;
-    scanf("%d %d %d %d",&a,&b,&c,&d);
-    int*a1=(int*)calloc(a,sizeof(int));
-    int*b1=(int*)calloc(b,sizeof(int));
-    int*c1=(int*)calloc(c,sizeof(int));
-    int*d1=(int*)calloc(d,sizeof(int));
-    if(a1==NULL || b1==NULL || c1==NULL || d1==NULL) {
-        free(a1);
-        free(b1);
-        free(c1);
-        free(d1);
-        return -1;
+
+int solve(int arr[],int n,int depth) {
+    if (n==1) {
+        if (equal(arr[0],24)) {
+            stepCount = depth;
+            return 1;
+        }
+        return 0;
+    } else {
+        for (int i=0;i<n;i++) {
+            for (int j=0;j<n;j++) {
+                if (i==j) continue;
+
+                int temp[4]={0};
+                int k=0;
+                for (int m=0;m<n;m++) {
+                    if (m!=i && m!=j) temp[k++]=arr[m];
+                }
+
+                // +
+                temp[k]=arr[i]+arr[j];
+                stepA[depth]=arr[i]; stepB[depth]=arr[j]; stepOp[depth]='+'; stepR[depth]=temp[k];
+                if (solve(temp,k+1,depth+1)) return 1;
+
+                // -
+                temp[k]=arr[i]-arr[j];
+                stepA[depth]=arr[i]; stepB[depth]=arr[j]; stepOp[depth]='-'; stepR[depth]=temp[k];
+                if (solve(temp,k+1,depth+1)) return 1;
+
+                // *
+                temp[k]=arr[i]*arr[j];
+                stepA[depth]=arr[i]; stepB[depth]=arr[j]; stepOp[depth]='*'; stepR[depth]=temp[k];
+                if (solve(temp,k+1,depth+1)) return 1;
+
+                // /
+                if (arr[j]!=0 && arr[i]%arr[j]==0) {
+                    temp[k]=arr[i]/arr[j];
+                    stepA[depth]=arr[i]; stepB[depth]=arr[j]; stepOp[depth]='/'; stepR[depth]=temp[k];
+                    if (solve(temp,k+1,depth+1)) return 1;
+                }
+            }
+        }
+        return 0;
     }
-    for(int i=0;i<a;i++) {
-        scanf("%d",&a1[i]);
+}
+
+int main(){
+    int arr[4]={0};
+    for (int i=0;i<4;i++) scanf("%d",&arr[i]);
+
+    int m=solve(arr,4,0);
+    if (m) {
+        for (int s=0;s<stepCount;s++) {
+            printf("%d%c%d=%d\n", stepA[s], stepOp[s], stepB[s], stepR[s]);
+        }
+    } else {
+        printf("No answer!\n");
     }
-    for(int j=0;j<b;j++) {
-        scanf("%d",&b1[j]);
-    }
-    for(int k=0;k<c;k++) {
-        scanf("%d",&c1[k]);
-    }
-    for(int l=0;l<d;l++) {
-        scanf("%d",&d1[l]);
-    }
-    int ma=0,mb=0,mc=0,md=0;
-    int mdp=0,mt=0,nm=0,sum=0;
-    for(int i=0;i<a;i++) {
-        sum+=a1[i];
-    }
-    ma=fun(0,a1,a,ma,nm,sum);
-     mdp=0;mt=0;nm=0;sum=0;
-    for(int i=0;i<b;i++) {
-        sum+=b1[i];
-    }
-    mb=fun(0,b1,b,mb,nm,sum);
-    mdp=0;mt=0;nm=0;sum=0;
-    for(int i=0;i<c;i++) {
-        sum+=c1[i];
-    }
-    mc=fun(0,c1,c,mc,nm,sum);
-    mdp=0;mt=0;nm=0;sum=0;
-    for(int i=0;i<d;i++) {
-        sum+=d1[i];
-    }
-    md=fun(0,d1,d,md,nm,sum);
-    printf("%d\n",ma+mb+mc+md);
-    free(a1);
-    free(b1);
-    free(c1);
-    free(d1);
     return 0;
 }
