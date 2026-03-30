61b45dbba81713bb93a70ba3ed469fbf2c4c9d14
hhxxttxsggg <16502221+hhxxttxsggg@user.noreply.gitee.com>
2026-03-30 18:26:50 +0800

    好的这个是括号匹配问题 但是遇到eof信号问题导致一直无法输出最后一行，然后用while循环的话，其实输入n是没有什么用的，这也就是问题，然后学了for(char c : line)这种表达方式，不知道用for循环会不会出现这样的问题

--- alg.cpp
+++ alg.cpp
@@ -1,24 +1,37 @@
-#include <queue>
+#include <stack>
+#include <string>
 #include <iostream>
 using namespace std;
+char tans(char c) {
+    if(c=='(') return ')';
+    if(c=='[') return ']';
+    if(c=='{') return '}';
+    return ' ';
+}
 int main() {
-     queue<int> student;
-    int n,m;
-    cin>>n>>m;
-    for(int i=1;i<n+1;i++) {
-        student.push(i);
-    }
-    int count=0;
-    int current;
-    while(!student.empty()) {
-        current=student.front();
-        student.pop();
-        count++;
-        if(count%m) {
-            student.push(current);
+     stack<char>bracket;
+    int n;
+    cin>>n;
+    string line;
+    cin.ignore();
+    while(getline(cin,line)) {
+        while(!bracket.empty())  bracket.pop();
+        for(int k=0; k<line.size(); k++){
+            if (bracket.empty()) {
+                bracket.push(line[k]);
+            }
+            else if (line[k]==tans(bracket.top())) {
+                bracket.pop();
+            }
+            else {
+                bracket.push(line[k]);
+            }
+        }
+        if(bracket.empty()) {
+            cout<<"YES"<<endl;
         }
         else {
-            cout <<current<< " ";
+            cout<<"NO"<<endl;
         }
     }
 return 0;
