b2847d61808832ed0c900d613054aa219711a598
hhxxttxsggg <16502221+hhxxttxsggg@user.noreply.gitee.com>
2026-03-31 19:26:14 +0800

    好吧，这个就是再之前的那个上面简化了，好像这个代码更简单dfs，好吧不简单，最初的办法超时了，只能改成bfs了，好的其实是我之前那个代码存在问题，我没有考虑到如果找不到路径那么那个只有一个没有找到的限制条件将会一直执行下去，最终导致超时，也幸亏有超时机制的判断，要不循环会导致卡死，这是用队列存储，我还不是很会用

--- alg.cpp
+++ alg.cpp
@@ -1,73 +1,63 @@
 #include <iostream>
 #include <vector>
-#include <stack>
+#include <queue>
 using namespace std;
 
-int n, m, t;
+int n, m;
 int sx, sy, fx, fy;
-long long ans = 0;
+bool reachable = false;
 
 vector<vector<int>> blocked;
 vector<vector<int>> visited;
-
 
 int dx[4] = {1, -1, 0, 0};
 int dy[4] = {0, 0, 1, -1};
 
 int main() {
-    cin >> n >> m >> t;
-    cin >> sx >> sy >> fx >> fy;
+    cin >> n >> m;
+    sx = 0, sy = 0, fx = n - 1, fy = m - 1;
 
-    blocked.assign(n + 1, vector<int>(m + 1, 0));
-     visited.assign(n + 1, vector<int>(m + 1, 0));
-    //这一行相当与声明这个数组有多大，这个数组是n+1个元素，每个元素是一个m+1大小的数组，数组中的每个元素初始值为0
+    blocked.assign(n, vector<int>(m, 0));
+    visited.assign(n, vector<int>(m, 0));
 
-    for (int i = 0; i < t; i++) {
-        int x, y;
-        cin >> x >> y;
-        blocked[x][y] = 1;
+    for (int i = 0; i < n; i++) {
+        for (int j = 0; j < m; j++) {
+            char c;
+            cin >> c;
+            blocked[i][j] = (c == '#');
+        }
     }
 
     if (blocked[sx][sy] || blocked[fx][fy]) {
-        cout << 0 << '\n';
-        return 0;//如果起点或者终点是障碍物，属于防御型
+        cout << "No" << endl;
+        return 0;
     }
 
-    struct Frame {
-        int x, y, dir; // dir: 下一个要尝试的方向 [0..4]
-    };
-
-    stack<Frame> st;
+    queue<pair<int, int>> q;
     visited[sx][sy] = 1;
-    st.push({sx, sy, 0});
+    q.push({sx, sy});
 
-    while (!st.empty()) {
-        Frame &cur = st.top();
+    while (!q.empty() && !reachable) {
+        auto [x, y] = q.front();
+        q.pop();
 
-        if (cur.x == fx && cur.y == fy) {
-            ans++;
-            visited[cur.x][cur.y] = 0; // 回溯算法，
-            st.pop();
-            continue;
+        if (x == fx && y == fy) {
+            reachable = true;
+            break;
         }
 
-        if (cur.dir == 4) {
-            visited[cur.x][cur.y] = 0; // 四个方向都试过，回溯
-            st.pop();
-            continue;
+        for (int i = 0; i < 4; i++) {
+            int nx = x + dx[i];
+            int ny = y + dy[i];
+
+            if (nx < 0 || nx >= n || ny < 0 || ny >= m) continue;
+            if (blocked[nx][ny] || visited[nx][ny]) continue;
+
+            visited[nx][ny] = 1;
+            q.push({nx, ny});
         }
-
-        int i = cur.dir++;
-        int nx = cur.x + dx[i];
-        int ny = cur.y + dy[i];
-
-        if (nx < 1 || nx > n || ny < 1 || ny > m) continue;
-        if (blocked[nx][ny] || visited[nx][ny]) continue;
-
-        visited[nx][ny] = 1;
-        st.push({nx, ny, 0});
     }
 
-    cout << ans << '\n';
+    cout << (reachable ? "Yes" : "No") << endl;
     return 0;
 }
