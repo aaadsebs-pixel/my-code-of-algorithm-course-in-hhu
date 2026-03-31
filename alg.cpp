f9840b208990cfd2e05180844338f8d8cb0f1c08
hhxxttxsggg <16502221+hhxxttxsggg@user.noreply.gitee.com>
2026-03-31 16:35:59 +0800

    给定一个 N×M 方格的迷宫，迷宫里有 T 处障碍，障碍处不可通过。
    在迷宫中移动有上下左右四种方式，每次只能移动一个方格。数据保证起点上没有障碍。
    
    给定起点坐标和终点坐标，每个方格最多经过一次，问有多少种从起点坐标到终点坐标的方案。
    
    输入格式
    第一行为三个正整数 N,M,T，分别表示迷宫的长宽和障碍总数。
    
    第二行为四个正整数 SX,SY,FX,FY。SX,SY 代表起点坐标，FX,FY 代表终点坐标。
    
    接下来 T 行，每行两个正整数，表示障碍点的坐标。
    好吧老师这次没有给代码示例我不知道2维的是怎么写所以我打算用注释的方式来理解代码
    这种用2维数组记录的方式，让dfs和bfs给人一种略带记忆的有向图的感觉，我认为精华部分就是先让visit值为0，然后再pop，这样这个点的访问记录就消失了，但是因为有向的存在，不会重复的走这条路了

--- alg.cpp
+++ alg.cpp
@@ -1,36 +1,73 @@
-#include <map>
-#include <string>
 #include <iostream>
+#include <vector>
+#include <stack>
 using namespace std;
+
+int n, m, t;
+int sx, sy, fx, fy;
+long long ans = 0;
+
+vector<vector<int>> blocked;
+vector<vector<int>> visited;
+
+
+int dx[4] = {1, -1, 0, 0};
+int dy[4] = {0, 0, 1, -1};
+
 int main() {
-    map<string, int> ds;
-    int n;
-    cin >> n;
-    for (int i = 0; i < n; i++) {
-        int op,num;
-        string name;
-        cin >> op;
-        if (op == 1) {
-            cin>>name>>num;
-            ds[name] = num;
-            if (ds.find(name)!=ds.end()) cout <<"ok"<<endl;
-        }
-        else if (op == 2) {
-            cin>>name;
-            if (ds.find(name)!=ds.end()) cout <<ds[name]<<endl;
-            else cout <<"Not found"<<endl;
-        }
-        else if (op == 3) {
-            cin>>name;
-            if (ds.find(name)!=ds.end()) {
-                ds.erase(name);
-                cout <<"Deleted successfully"<<endl;
-            }
-            else cout <<"Not found"<<endl;
-        }
-        else if (op == 4) {
-            cout <<ds.size()<<endl;
-        }
+    cin >> n >> m >> t;
+    cin >> sx >> sy >> fx >> fy;
+
+    blocked.assign(n + 1, vector<int>(m + 1, 0));
+     visited.assign(n + 1, vector<int>(m + 1, 0));
+    //这一行相当与声明这个数组有多大，这个数组是n+1个元素，每个元素是一个m+1大小的数组，数组中的每个元素初始值为0
+
+    for (int i = 0; i < t; i++) {
+        int x, y;
+        cin >> x >> y;
+        blocked[x][y] = 1;
     }
+
+    if (blocked[sx][sy] || blocked[fx][fy]) {
+        cout << 0 << '\n';
+        return 0;//如果起点或者终点是障碍物，属于防御型
+    }
+
+    struct Frame {
+        int x, y, dir; // dir: 下一个要尝试的方向 [0..4]
+    };
+
+    stack<Frame> st;
+    visited[sx][sy] = 1;
+    st.push({sx, sy, 0});
+
+    while (!st.empty()) {
+        Frame &cur = st.top();
+
+        if (cur.x == fx && cur.y == fy) {
+            ans++;
+            visited[cur.x][cur.y] = 0; // 回溯算法，
+            st.pop();
+            continue;
+        }
+
+        if (cur.dir == 4) {
+            visited[cur.x][cur.y] = 0; // 四个方向都试过，回溯
+            st.pop();
+            continue;
+        }
+
+        int i = cur.dir++;
+        int nx = cur.x + dx[i];
+        int ny = cur.y + dy[i];
+
+        if (nx < 1 || nx > n || ny < 1 || ny > m) continue;
+        if (blocked[nx][ny] || visited[nx][ny]) continue;
+
+        visited[nx][ny] = 1;
+        st.push({nx, ny, 0});
+    }
+
+    cout << ans << '\n';
     return 0;
 }
