7312d9ed9624a52b7edd57d1d20d5d823e50d357
hhxxttxsggg <16502221+hhxxttxsggg@user.noreply.gitee.com>
2026-04-01 19:32:46 +0800

    这个题和这那个题差不多，最短路径就找bfs嘛

--- alg.cpp
+++ alg.cpp
@@ -1,86 +1,39 @@
 #include <iostream>
 #include <vector>
-#include <stack>
 #include <queue>
-#include <algorithm>
 using namespace std;
 
-vector<vector<int>> adj;
-vector<bool> vis1, vis2;
+int main() {
+    int N, A, B;
+    cin >> N >> A >> B;
 
-void dfs(int start) {
-    stack<int> s;
-    s.push(start);
-    vis1[start] = true;
-    cout << start << " ";
+    vector<int> K(N + 1);
+    for (int i = 1; i <= N; ++i) cin >> K[i];
 
-    while (!s.empty()) {
-        int current = s.top();
-        bool found = false;
-
-        for (int i = 0; i < (int)adj[current].size(); i++) {
-            int neighbor = adj[current][i];
-            if (!vis1[neighbor]) {
-                s.push(neighbor);
-                vis1[neighbor] = true;
-                cout << neighbor << " ";
-                found = true;
-                break;
-            }
-        }
-
-        if (!found) {
-            s.pop();
-        }
-    }
-    cout << "\n";
-}
-
-void bfs(int start) {
+    vector<int> dist(N + 1, -1);
     queue<int> q;
-    q.push(start);
-    vis2[start] = true;
-    cout << start << " ";
+
+    dist[A] = 0;
+    q.push(A);
 
     while (!q.empty()) {
-        int current = q.front();
+        int cur = q.front();
         q.pop();
 
-        for (int neighbor : adj[current]) {
-            if (!vis2[neighbor]) {
-                q.push(neighbor);
-                vis2[neighbor] = true;
-                cout << neighbor << " ";
-            }
+        int up = cur + K[cur];
+        int down = cur - K[cur];
+
+        if (up >= 1 && up <= N && dist[up] == -1) {
+            dist[up] = dist[cur] + 1;
+            q.push(up);
+        }
+        if (down >= 1 && down <= N && dist[down] == -1) {
+            dist[down] = dist[cur] + 1;
+            q.push(down);
         }
     }
-    cout << "\n";
-}
 
-int main() {
-    ios::sync_with_stdio(false);
-    cin.tie(nullptr);
-
-    int n, m; // n: 边数, m: 点数(点编号 1..m)
-    cin >> n >> m;
-
-    adj.resize(m + 1);
-    vis1.assign(m + 1, false);
-    vis2.assign(m + 1, false);
-
-    for (int i = 1; i <= m; i++) {
-        int a, c;
-        cin >> a >> c;
-        adj[a].push_back(c);     // 有向图
-        // adj[c].push_back(a);  // 如果是无向图，取消注释
-    }
-
-    for (int i = 1; i <= m; i++) {
-        sort(adj[i].begin(), adj[i].end());
-    }
-
-    dfs(1);
-    bfs(1);
-
+    cout << dist[B] << '\n';
     return 0;
 }
+
